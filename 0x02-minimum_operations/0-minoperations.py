e51c890b2364d557ddfea69e4000d3944fad98e4
Tsumee <tsumeemoloisi@gmail.com>
2024-03-21 14:02:44 +0000

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Method of determining the number of minmum operations given to characters n
+"""
+
+def minOperations(n):
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
